b2aadaa2a01703a0de57cc5ac83fa0a179d0c9ae
alexanderlindquis <143742409+alexanderlindquis@users.noreply.github.com>
2024-09-04 14:36:56 -0400

    Add files via upload

--- Lab2/main.c
+++ Lab2/main.c
@@ -1,1 +1,16 @@
+#include "pico/stdlib.h"
+#include "sw_in.h"
+#include "led_out.h"
 
+main(){
+    bool in1, in2;
+    uint16_t out;
+    sw_in_init();
+    led_out_init();
+    while(1) {
+        in1 = sw_in_read1();
+        in2 = sw_in_read2();
+        //Compute 'out'
+        led_out_write(out);
+    }
+}
