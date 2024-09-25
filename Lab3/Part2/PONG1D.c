8672ad53f46ef73e0c8704db8c94a6cf3202d7f0
alexanderlindquis <143742409+alexanderlindquis@users.noreply.github.com>
2024-09-25 10:13:05 -0400

    Update PONG1D.c
    Completed State Machine and Game Logic >> May require more work to integrate UART functionality and needs to be unit tested.

--- Lab3/Part2/PONG1D.c
+++ Lab3/Part2/PONG1D.c
@@ -9,16 +9,20 @@ int main(){
   tBtn = timer_read();
   tPong = timer_read();
   uint32 PONGPD_MS = 300;
+  const uint32_t MASK_9_2 = 0x000003fc;
+  uint32_t outval = 0x1;
+  gpio_init_mask(MASK_9_2);
+  gpio_set_dir_out_masked(MASK_9_2);
   while(true){
     tCurrent = timer_read();
+    gpio_put_masked(MASK_9_2, outval << 2);
     if(timer_elapsed_ms(tBtn, tCurrent) >= DEBOUNCEPD_MS){
       debounce_sw1_tick();
       deounce_sw2_tick();
     }
-    if(debounce_sw1_pressed() || timer_elapsed_ms(tPong, tCurrent) >= PONGPD_MS){
+    if(debounce_sw1_pressed() || debounce_sw2_pressed() || timer_elapsed_ms(tPong, tCurrent) >= PONGPD_MS){
       pong_tick();
     }
-    
   }
 }
 
@@ -36,13 +40,59 @@ void pong_tick(){
       }
       break;
     case LEFT:
-      
+      if(debounce_sw1_pressed()){
+        if(outval == 0x80){
+          PONG_State = RIGHT;
+          P0 = false;
+          outval = outval >> 1;
+        }
+        else{
+          PONG_State = BLINK;
+        }
+      }
+      else if(outval <= 0x80){
+        outval = outval << 1;
+      }
+      else{
+        PONG_State = BLINK;
+      }
       break;
     case RIGHT:
-      
+      if(debounce_sw2_pressed()){
+        if(outval == 0x1){
+          PONG_State = LEFT;
+          P0 = true;
+          outval = outval << 1;
+        }
+        else{
+          PONG_State = BLINK;
+        }
+      }
+      else if(outval >= 0x1){
+        outval = outval >> 1;
+      }
+      else{
+        PONG_State = BLINK;
+      }
       break;
     case BLINK:
-      
+      if(P0){
+        outval = 0x1;
+      }
+      else{
+        outval = 0x80;
+      }
+      for(int i = 0; i < 10; i++){
+        if(i % 2 == 0){
+          gpio_put_masked(MASK_9_2, outval << 2);
+        }
+        else{
+          gpio_put_masked(MASK_9_2, 0x0);
+        }
+        sleep_ms(100);
+      }
+      P0 = !P0;
+      PONG_State = SERVE;
       break;
   }
 }
