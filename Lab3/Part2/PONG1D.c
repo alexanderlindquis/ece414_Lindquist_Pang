8d38d7363630f58a79b0f7a4a0ccd6024dc7fe51
Alexander Lindquist <lindquia@ad.lafayette.edu>
2024-10-07 14:57:45 -0400

    Final Version

--- Lab3/Part2/PONG1D.c
+++ Lab3/Part2/PONG1D.c
@@ -6,18 +6,119 @@
 #include "debounce_sw2.h"
 #include "led_out.h"
 #include "timer.h"
+#include "hardware/uart.h"
 #define LED_PIN 2
-#define LED_PIN2 10
-#define PONGPD_MS 300
+#define LED_PIN2 9
+#define UART_ID uart0
+#define BAUD_RATE 115200
+#define UART_TX_PIN 0
+#define UART_RX_PIN 1
+
 
 static enum PONG_States {LEFT, RIGHT, SERVE, BLINKL,BLINKR} PONG_State;
 const uint32_t MASK_9_2 = 0x000003fc;
 uint32_t outval = 0x80;
 bool P0 = false;
+
+uint32_t PONGPD_MS = 300;
+
+
+
+
+//Update Game State based on button presses, current active player, 
+void pong_tick(){
+  switch(PONG_State){
+    case SERVE:
+    PONGPD_MS = 300;
+      if(P0){
+        outval = 0x80;
+      }else 
+        outval = 0x01;
+        
+      if(P0 && debounce_sw1_pressed()){
+        PONG_State = LEFT;
+
+      }
+      else if(!P0 && debounce_sw2_pressed()){
+        PONG_State = RIGHT;
+      }
+      break;
+
+    case LEFT:
+      outval = outval >> 1;
+      
+      if (outval == 0x01) {
+        if (debounce_sw2_pressed()) {
+          printf("BAM\n");
+          PONG_State = RIGHT;
+          if(PONGPD_MS>100){
+        PONGPD_MS = PONGPD_MS - 50;
+        }
+        } else {
+          PONG_State = BLINKL;
+        }
+      } else if (debounce_sw2_pressed()) {
+        PONG_State = BLINKL;
+      }
+      break;
+    case RIGHT:
+        outval = outval << 1;
+        if (outval == 0x80) {
+          if (debounce_sw1_pressed()) {
+            printf("POW\n");
+            PONG_State = LEFT;
+            if(PONGPD_MS>100){
+            PONGPD_MS = PONGPD_MS - 50;
+            }
+          } else {
+            PONG_State = BLINKR;
+          }
+        } else if (debounce_sw1_pressed()) {
+          PONG_State = BLINKR;
+        }
+        break;
+
+    case BLINKL:
+        printf("L Wins\n");
+        gpio_put(LED_PIN2, 1);
+        sleep_ms(100);
+        gpio_put(LED_PIN2, 0);
+        sleep_ms(100);
+        gpio_put(LED_PIN2, 1);
+        sleep_ms(100);
+        gpio_put(LED_PIN2, 0);
+        sleep_ms(100);
+        P0 = true;
+        printf("L\n");
+        
+        PONG_State = SERVE;
+      break;
+
+    case BLINKR:
+        printf("R Wins\n");
+        gpio_put(LED_PIN, 1);
+        sleep_ms(100);
+        gpio_put(LED_PIN, 0);
+        sleep_ms(100);
+        gpio_put(LED_PIN, 1);
+        sleep_ms(100);
+        gpio_put(LED_PIN, 0);
+        sleep_ms(100);
+        P0 = false;
+        
+        printf("R\n");
+        PONG_State = SERVE;
+      break;
+  }
+}
 //main logic
 int main(){
+  stdio_init_all();
+  printf("R\n");
   gpio_init(LED_PIN);
   gpio_set_dir(LED_PIN, GPIO_OUT);
+  gpio_init(LED_PIN2);
+  gpio_set_dir(LED_PIN2, GPIO_OUT);
   sw_in_init();
   debounce_sw1_init();
   debounce_sw2_init();
@@ -48,74 +149,5 @@ int main(){
       pong_tick();
       tPong = tPong2;
     }
-  }
-}
-
-//Update Game State based on button presses, current active player, 
-void pong_tick(){
-  switch(PONG_State){
-    case SERVE:
-      if(P0){
-        outval = 0x80;
-      }else 
-        outval = 0x01;
-      if(P0 && debounce_sw1_pressed()){
-        PONG_State = LEFT;
-      }
-      else if(!P0 && debounce_sw2_pressed()){
-        PONG_State = RIGHT;
-      }
-      break;
-
-    case LEFT:
-      outval = outval >> 1;
-      if (outval == 0x01) {
-        if (debounce_sw2_pressed()) {
-          PONG_State = RIGHT;
-        } else {
-          PONG_State = BLINKL;
-        }
-      } else if (debounce_sw2_pressed()) {
-        PONG_State = BLINKL;
-      }
-      break;
-    case RIGHT:
-        outval = outval << 1;
-        if (outval == 0x80) {
-          if (debounce_sw1_pressed()) {
-            PONG_State = LEFT;
-          } else {
-            PONG_State = BLINKR;
-          }
-        } else if (debounce_sw1_pressed()) {
-          PONG_State = BLINKR;
-        }
-        break;
-
-    case BLINKL:
-        gpio_put(LED_PIN2, 1);
-        sleep_ms(100);
-        gpio_put(LED_PIN2, 0);
-        sleep_ms(100);
-        gpio_put(LED_PIN2, 1);
-        sleep_ms(100);
-        gpio_put(LED_PIN2, 0);
-        sleep_ms(100);
-        P0 = true;
-        PONG_State = SERVE;
-      break;
-
-    case BLINKR:
-        gpio_put(LED_PIN, 1);
-        sleep_ms(100);
-        gpio_put(LED_PIN, 0);
-        sleep_ms(100);
-        gpio_put(LED_PIN, 1);
-        sleep_ms(100);
-        gpio_put(LED_PIN, 0);
-        sleep_ms(100);
-        P0 = false;
-        PONG_State = SERVE;
-      break;
   }
 }
--- Lab3/led_out.h
+++
@@ -1,8 +0,0 @@
-#ifndef LED_OUT_H
-#define LED_OUT_H
-
-#include "pico/stdlib.h"
-
-void led_out_init();
-void led_out_write(bool val);
-#endif
