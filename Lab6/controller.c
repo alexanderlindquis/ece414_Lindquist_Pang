95bd7525f6e630c9eb0071d1390728e877e249fa
alexanderlindquis <143742409+alexanderlindquis@users.noreply.github.com>
2024-11-04 13:36:16 -0500

    Update controller.c

--- Lab6/controller.c
+++ Lab6/controller.c
@@ -11,6 +11,17 @@ float IntegMax = 200;
 float IntegMin = 0;
 float actualPrev, Integ = 0;
 float desired;
+float pwm_level;
+
+static inline int32_t max (uint32_t a, uint32_t b) {
+    if (a > b) return a;
+    else return b;
+}
+
+static inline int32_t min (uint32_t a, uint32_t b) {
+    if (a < b) return a;
+    else return b;
+}
 
 float actuator_calc(){
     float actual = ic_getrpm();
@@ -28,4 +39,20 @@ float actuator_calc(){
         Integ = IntegMin;
     }
     return Kp * Error + Ki * Integ - Kd * Deriv;
+}
+
+void controller_init(){
+    pwm_level = 0x8000;
+    pwm_pin_init(pwm_level);
+}
+
+void controller_tick(){
+    uint16_t bias = (int)actuator_calc();
+    if(bias < 0){
+        pwm_level = max(pwm_level + bias, 0);
+    }
+    else if(bias > 0){
+        pwm_level = min(pwm_level + bias, 0xffff);
+    }
+    pwm_pin_set_level(pwm_level);
 }
