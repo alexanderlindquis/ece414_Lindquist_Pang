1be1f255ebb98e61b9fb510e988593022371c580
Steven Pang <pangl@lafayette.edu>
2024-11-06 12:54:01 -0500

    lcd display

---
+++ Lab6/lcd.c
@@ -0,0 +1,50 @@
+#include <stdio.h>         
+#include <math.h>         
+#include "pico/stdlib.h"   
+#include "pico/rand.h"     
+#include "TFTMaster.h"
+#include "lcd.h"
+#include "timer.h"
+char buffer[10];
+uint32_t time_start, time_b;
+
+void lcd_init(){
+    stdio_init_all();
+    tft_init_hw();
+    tft_begin();
+    tft_setRotation(3);
+    tft_fillScreen(ILI9340_BLACK);
+    tft_setTextColor(ILI9340_BLUE);
+    tft_setTextSize(2);
+    time_start = timer_read();
+}
+void dpcommand(){
+    if(uart_is_readable(uart0)){
+        tft_setCursor(0,0);
+        tft_setTextColor(ILI9340_WHITE);
+        tft_writeString(c)
+    }
+}
+void dprpm(){
+    uint32_t rpm = ic_getrpm();
+    tft_setCursor(300,0);
+    tft_setTextColor(ILI9340_WHITE);
+    sprintf(buffer, "RPM =%u ",rpm);
+    tft_writeString(buffer);
+    if(!NO_CAPTURE_READY){
+        time_b = timer_read();
+        uint32_t elapsed = (timer_elapsed_ms(time_start,time_b));
+        uint16_t y = (3000-rpm)/20; 
+        uint16_t x = elapsed/10;
+        
+        tft_drawPixel(x,y,ILI9340_RED);
+        tft_drawPixel(x,desire,ILI9340_GREEN);
+        tft_drawPixel(x,pwm,ILI9340_BLUE);
+        if(x>=300){
+            tft_fillScreen(ILI9340_BLACK);
+            time_start = timer_read;
+        }
+        if (y < 0) y = 0;
+        if (y >= 240) y = 239;
+    }
+   }
---
+++ Lab6/lcd.h
@@ -0,0 +1,21 @@
+#ifndef lcd.h
+#define lcd.h
+
+#include <stdio.h>         
+#include <math.h>         
+#include "pico/stdlib.h"   
+#include "pico/rand.h"     
+#include "TFTMaster.h
+#include "ic.h"
+#include "
+
+
+void lcd_init();
+
+void dpcommand();
+
+void dprpm();
+
+
+
+#endif
